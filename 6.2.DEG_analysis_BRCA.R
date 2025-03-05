b179db62882a61029859eae9f261deb0a905764e
camillabm <camilla.bruckmann@outlook.com>
2025-03-05 09:24:48 -0300

    6.2.DEG_analysis_BRCA.R

--- 6.2.DEG_analysis_BRCA.R
+++ 6.2.DEG_analysis_BRCA.R
@@ -2,26 +2,18 @@
 ### DEGs analysis ###
 #####################
 
-######## GBM ########
+######## BRCA ########
 
 # Libraries
 if (!require("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
-BiocManager::install(c("DESeq2", "EnhancedVolcano"))
+BiocManager::install(c("DESeq2", "biomaRt", "limma"))
 install.packages("dplyr")
-install.packages("readxl")
-install.packages("fpc")
-install.packages("fmsb")
 
 library(dplyr)
 library(DESeq2)
-library(pheatmap)
-library(readxl)
 library (biomaRt)
-library(ggplot2)
-library (EnhancedVolcano)
-library(RColorBrewer)
-library(fmsb)
+library (limma)
 
 # Data identification 
 expression_analysis_brca <- read.csv("2.0/Expression Pan/Expression/cohort/matrix_brcaR_comb.csv", row.names = 1) #Previous expression table (genes x samples)
@@ -79,69 +71,3 @@ nrow(res_sig_brca) #Annotate result
 
 # Save differential expression data as .csv
 write.csv(res_sig_brca, "2.0/Expression Pan/Results/tables/result_brcaR_ARG.csv", row.names = FALSE)
-
-# Volcano Plot
-volcano_plot <- EnhancedVolcano(res_df_brca, 
-                                lab = rownames(res_df_brca),
-                                x = 'log2FoldChange',  
-                                y = 'padj',            
-                                title = 'Differential Expression Analysis',
-                                pCutoff = 0.01,        
-                                FCcutoff = 1.5,          
-                                pointSize = 3,         
-                                labSize = 0,           
-                                drawConnectors = FALSE, 
-                                widthConnectors = 0.5) 
-
-
-ggsave("2.0/Expression Pan/Results/figures/volcano_plot_brcaR_ARG.png", plot = volcano_plot, width = 8, height = 6, dpi = 300)
-
-# UMAP
-library(umap)
-library(uwot)
-
-vst_brca <- varianceStabilizingTransformation(dds_specific_brca, blind = TRUE)
-expr_brca <- assay(vst_brca)
-metadata_brca <- colData(dds_specific_brca)
-metadata_brca <- as.data.frame(metadata_brca)
-
-set.seed(42)  # Para reprodutibilidade
-
-umap_result <- umap(
-  t(expr_brca),   # Matriz de expressão (genes x amostras)
-  n_neighbors = 15,  # Ajusta o número de vizinhos (valores menores = mais locais, valores maiores = mais globais)
-  min_dist = 0.001,  # Controla a dispersão dos pontos (menor = clusters mais compactos, maior = mais espalhado)
-  spread =  2,  # Controla a separação dos grupos
-  metric = "correlation",  # Pode testar "euclidean", "correlation" ou "cosine"
-  n_components = 2,  # Número de dimensões do UMAP
-  learning_rate = 0.1,
-  fast_sgd = TRUE,  # Torna o cálculo mais rápido
-  verbose = TRUE  # Mostra informações durante o processo
-)
-
-umap_df <- data.frame(
-  UMAP1 = umap_result[,1],
-  UMAP2 = umap_result[,2],
-  Condition = metadata_brca$condition,  # Ou outra variável relevante
-  Batch = metadata_brca$project  # Para verificar efeito do batch
-)
-
-colors <- c(
-  "Normal" = "gray70",  
-  "Tumor" = "#4682B4")
-
-UMAP <- ggplot(umap_df, aes(x = UMAP1, y = UMAP2, color = Condition)) +
-  geom_point(size = 2, alpha = 0.7) +  # Pontos maiores e mais suaves
-  scale_color_manual(values = colors) +  # Aplicar as cores definidas
-  theme_minimal(base_family = "Arial") +  # Fonte Arial para remover serifas
-  theme(
-    plot.title = element_text(size = 16, face = "bold"),
-    legend.title = element_text(size = 12),
-    legend.text = element_text(size = 10)
-  ) +
-  labs(
-    title = "UMAP - Visualização Melhorada",
-    color = "Condição"
-  )
-
-ggsave("2.0/Expression Pan/Results/Figures/UMAP_brcaR_ARG.png", plot = UMAP, width = 8, height = 6, dpi = 300, bg = "white")
